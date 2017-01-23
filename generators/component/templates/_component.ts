fbf94246939f6cb337d2286cbb61adde89eaa59d
Francisco Poleo <francjpd>
2017-01-23 22:00:32 +0100

    (fix): components

--- generators/component/templates/_component.ts
+++ generators/component/templates/_component.ts
@@ -1,8 +1,26 @@
 /// <reference types="angular"/>
 
+import angular = require('angular');
+
 import <%= ctrl %> from './<%= name %>.controller';
 
 let template = require('ngtemplate!html!./<%= name %>.template.html');
+
+class <%=name %>ComponentBindings{
+    private static instance:any;
+    isWorking:string;
+
+    constructor(){
+        this.isWorking = '<';
+    }
+
+    static getInstance(): <%= name %>ComponentBinding{
+        if(!angular.isDefined(this.instance)){
+            this.instance = new <%= name %>ComponentBinding();
+        }
+        return this.instance;
+    }
+}
 
 export default class <%= className %> implements ng.IComponentOptions{
 
@@ -19,6 +37,7 @@ export default class <%= className %> implements ng.IComponentOptions{
     constructor(){
         this.templateUrl= template;
         this.controller= <%= ctrl %>;
+        this.bindings = <%=name %>ComponentBindings.getInstance();
     }
 
 
--- generators/component/templates/_controller.ts
+++ generators/component/templates/_controller.ts
@@ -1,11 +1,23 @@
 ///<reference types="angular"/>
 
+interface I<%= name %>{
+    isWorking:boolean;
+}
 
-export default class <%= ctrl  %> implements ng.IComponentController{
+interface I<%= name %>ComponentController extends I<%= name %> {
+	doSomething():string;
+	
+}
+export default class <%= ctrl  %> implements I<%= name %>ComponentController{
+    static $inject :string[] = ['$rootScope'];
+
     constructor(){
         
     }
     public static getInstance():<%= ctrl  %>{
         return new <%= ctrl  %>();
+    }
+    $onInit(){
+        console.log('Need to modify <%= name %>Controller $rootScope', $rootScope)
     }
 }
